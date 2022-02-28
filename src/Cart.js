e56dc63bc02fec1bc0ef5be5f174912300892c69
Sakshi <sakshiga06@gmail.com>
2022-02-28 18:22:29 +0530

    props

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,88 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 99,
+                    title: 'Watch',
+                    qty: 1,
+                    img: '',
+                    id: 1
+                },
+                {
+                    price: 999,
+                    title: 'Phone',
+                    qty: 3,
+                    img: '',
+                    id: 2
+                },
+                {
+                    price: 999,
+                    title: 'Laptop',
+                    qty: 1,
+                    img: '',
+                    id: 3
+                }
+            ]
+        }
+    }
+
+    incQty = (product) => {
+        const {products} = this.state;
+        const index = products.indexOf(product);
+
+        products[index].qty += 1;
+
+        this.setState({
+            products
+        });
+    }
+
+    decQty = (product) => {
+        const {products} = this.state;
+        const index = products.indexOf(product);
+
+        if (products[index].qty === 0){
+            return;
+        }
+
+        products[index].qty -= 1;
+
+        this.setState({
+            products
+        });
+    }
+    
+    delProduct = (id) => {
+        const {products} = this.state;
+
+        const items = products.filter((item) => item.id !== id);
+
+        this.setState({
+            products: items
+        });
+    }
+
     render() {
+        const {products} = this.state;
         return (
             <div className="cart">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {
+                    products.map((product) => {
+                        return (
+                            <CartItem
+                                product={product}
+                                key={product.id}
+                                onIncQty={this.incQty}
+                                onDecQty={this.decQty}
+                                onDelete={this.delProduct}
+                            />
+                        );
+                    })
+                }
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,40 +1,15 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            price: 999,
-            title: 'Phone',
-            qty: 1,
-            img: ''
-        }
-    }
-
-    incQty = () => {
-        this.setState((prev) => {
-            return {
-                qty: prev.qty + 1
-            }
-        });
-    }
-
-    decQty = () => {
-        const {qty} = this.state;
-
-        if(qty === 0) {
-            return;
-        }
-        
-        this.setState((prev) => {
-            return {
-                qty: prev.qty - 1
-            }
-        });
-    }
 
     render() {
-        const {price, title, qty, img} = this.state;
+        const {price, title, qty, img} = this.props.product;
+        const {
+            product,
+            onIncQty,
+            onDecQty,
+            onDelete
+        } = this.props;
         return(
             <div className='cart-item'>
                 <div className='left-block'>
@@ -49,18 +24,19 @@ class CartItem extends React.Component {
                             alt='increase'
                             className='action-icons'
                             src='https://cdn-icons-png.flaticon.com/512/992/992651.png'
-                            onClick={this.incQty}
+                            onClick={() => onIncQty(product)}
                         />
                         <img 
                             alt='decrease'
                             className='action-icons'
                             src='https://cdn-icons-png.flaticon.com/512/992/992683.png'
-                            onClick={this.decQty}
+                            onClick={() => onDecQty(product)}
                         />
                         <img 
                             alt='delete'
                             className='action-icons'
                             src='https://cdn-icons-png.flaticon.com/512/3096/3096687.png'
+                            onClick={() => onDelete(product.id)}
                         />
                     </div>
                 </div>
